99cce53883740dc6093596802433d060341c3906
bakukiela <130473076+bakukiela@users.noreply.github.com>
2023-09-04 21:39:45 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
 import { DefaultUserProvider } from "./context/defaultUserContext";
