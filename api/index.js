b9769736dd0ced5daa955db21d2178477fbba7fc
bakukiela <bakukiela@gmail.com>
2023-09-11 20:43:51 +0200

    Deploy

--- api/index.js
+++ api/index.js
@@ -20,14 +20,15 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env,
+    // origin: "http://localhost:3000",
   })
 );
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../client/docs/upload");
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
@@ -38,8 +39,12 @@ const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
   const file = req.file;
+
   res.status(200).json(file.filename);
 });
+
+
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
